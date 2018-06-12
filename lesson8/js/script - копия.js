42be36646f53354974ae07fa97c9634bf4f1a1a7
Victor <victor.meriacri@mail.ru>
2018-06-12 21:46:31 +0300

    16th commit

--- lesson8/js/script - копия.js
+++ lesson8/js/script - копия.js
@@ -95,21 +95,29 @@ hideTabs(1);
 	}
 
 
-function smoothScroll(anchor, duration){
+	(function() {
 
+    
+
+        // Function to animate the scroll
+        var smoothScroll = function (anchor, duration) {
+
+            // Calculate how far and how fast to scroll
             var startLocation = window.pageYOffset;
             var endLocation = anchor.offsetTop;
             var distance = endLocation - startLocation;
             var increments = distance/(duration/16);
             var stopAnimation;
 
+            // Scroll the page by an increment, and check if it's time to stop
             var animateScroll = function () {
                 window.scrollBy(0, increments);
                 stopAnimation();
             };
 
+            // If scrolling down
             if ( increments >= 0 ) {
-
+                // Stop animation when you reach the anchor OR the bottom of the page
                 stopAnimation = function () {
                     var travelled = window.pageYOffset;
                     if ( (travelled >= (endLocation - increments)) || ((window.innerHeight + travelled) >= document.body.offsetHeight) ) {
@@ -117,9 +125,9 @@ function smoothScroll(anchor, duration){
                     }
                 };
             }
-
+            // If scrolling up
             else {
-
+                // Stop animation when you reach the anchor OR the top of the page
                 stopAnimation = function () {
                     var travelled = window.pageYOffset;
                     if ( travelled <= (endLocation || 0) ) {
@@ -128,28 +136,41 @@ function smoothScroll(anchor, duration){
                 };
             }
 
+            // Loop the animation function
             var runAnimation = setInterval(animateScroll, 16);
        
         };
 
+        // Define smooth scroll links
         	let menu = document.getElementsByTagName('ul')[0],
 							scrollToggle = menu.getElementsByTagName('a');
 
+        // For each smooth scroll link
         [].forEach.call(scrollToggle, function (toggle) {
 
+            // When the smooth scroll link is clicked
             toggle.addEventListener('click', function(e) {
+
+                // Prevent the default link behavior
                 e.preventDefault();
+
+                // Get anchor link and calculate distance from the top
                 var dataID = toggle.getAttribute('href');
                 var dataTarget = document.querySelector(dataID);
 
+                // If the anchor exists
                 if (dataTarget) {
+                    // Scroll to the anchor
                     smoothScroll(dataTarget, 500);
                 }
 
             }, false);
 
         });
- });
 
+    
 
+ })();
+
+});
 
