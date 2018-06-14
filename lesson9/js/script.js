3f065dc87336ab645f8ec4274a9a97833fac463e
Victor <victor.meriacri@mail.ru>
2018-06-14 20:12:32 +0300

    17th commit

---
+++ lesson9/js/script.js
@@ -0,0 +1,199 @@
+window.addEventListener('DOMContentLoaded', function(){
+
+	let tab = document.getElementsByClassName('info-header-tab'),
+			tabContent = document.getElementsByClassName('info-tabcontent'),
+			info = document.getElementsByClassName('info-header')[0];
+
+	function hideTabs(a){
+		for (let i = a; i < tabContent.length; i++) {
+			tabContent[i].classList.remove('show');
+			tabContent[i].classList.add('hide');
+		}
+	}
+
+hideTabs(1);
+
+	function showTabs(b) {
+		if (tabContent[b].classList.contains('hide')) {
+			hideTabs(0);
+			tabContent[b].classList.remove('hide');
+			tabContent[b].classList.add('show');
+		}
+	}
+
+	info.addEventListener('click', function(event){
+		let target = event.target;
+			if (target.className == 'info-header-tab') {
+				for (let i = 0; i < tab.length; i++){
+					if ( target == tab[i]) {
+						showTabs(i);
+						break;
+					}
+				}
+				}			
+	});
+
+
+	
+	let deadline = '2018-06-15';
+
+	function getTimeRemaining(endTime) {
+		let t = Date.parse(endTime) - Date.parse(new Date()),
+				seconds = addZero (Math.floor( t / 1000 % 60)),
+				minutes = addZero (Math.floor( t / 1000 / 60 % 60)),
+				hours = addZero(Math.floor( t / (1000 * 60 * 60)));
+
+				if (t == 0 || t < 0){
+					return {
+					'total': 0,
+					};
+				} else {
+					return {
+					'total': t,
+					'hours': hours,
+					'minutes': minutes,
+					'seconds': seconds
+
+				};
+				}
+				
+	};
+
+	function setClock(id, endTime) {
+
+		let timer = document.getElementById(id);
+		    hours = document.querySelector('.hours'),
+			  minutes = document.querySelector('.minutes'),
+			  seconds = document.querySelector('.seconds');
+
+			  function updateClock() {
+
+			  	let t = getTimeRemaining(endTime);
+			  		if (t.total == 0) {
+			  		clearInterval(timeInterval);
+			  		hours.innerHTML = '00',
+			  		minutes.innerHTML = '00',
+			  		seconds.innerHTML = '00';
+			  		} else {
+			  		hours.innerHTML = t.hours,
+			  		minutes.innerHTML = t.minutes,
+			  		seconds.innerHTML = t.seconds;
+			  		}	
+			  }
+
+			  let timeInterval = setInterval(updateClock, 1000);
+	}
+
+	setClock('timer', deadline);
+
+
+	function addZero(n) {
+		if (n < 10) {
+		n = '0' + n;	
+		}
+		return n
+	}
+
+
+function smoothScroll(anchor, duration){
+
+            var startLocation = window.pageYOffset;
+            var endLocation = anchor.offsetTop;
+            var distance = endLocation - startLocation;
+            var increments = distance/(duration/16);
+            var stopAnimation;
+
+            var animateScroll = function () {
+                window.scrollBy(0, increments);
+                stopAnimation();
+            };
+
+            if ( increments >= 0 ) {
+
+                stopAnimation = function () {
+                    var travelled = window.pageYOffset;
+                    if ( (travelled >= (endLocation - increments)) || ((window.innerHeight + travelled) >= document.body.offsetHeight) ) {
+                        clearInterval(runAnimation);
+                    }
+                };
+            }
+
+            else {
+
+                stopAnimation = function () {
+                    var travelled = window.pageYOffset;
+                    if ( travelled <= (endLocation || 0) ) {
+                        clearInterval(runAnimation);
+                    }
+                };
+            }
+
+            var runAnimation = setInterval(animateScroll, 16);
+       
+        };
+
+        	let menu = document.getElementsByTagName('ul')[0],
+							scrollToggle = menu.getElementsByTagName('a');
+
+        [].forEach.call(scrollToggle, function (toggle) {
+
+            toggle.addEventListener('click', function(e) {
+                e.preventDefault();
+                var dataID = toggle.getAttribute('href');
+                var dataTarget = document.querySelector(dataID);
+
+                if (dataTarget) {
+                    smoothScroll(dataTarget, 500);
+                }
+
+            }, false);
+
+        });
+
+
+ let more =  document.querySelector('.more'),
+ 		 overlay = document.querySelector('.overlay'),
+ 		 popup = document.querySelector('.popup-close'),
+ 		 desc_btn = document.querySelectorAll('.description-btn');
+
+ 		 more.addEventListener('click', function(){
+ 		 	 if (isIE()){
+				console.log('Ты используешь: Internet Explorer');
+ 		 	 } else {
+ 		 		overlay.style.display = 'block';
+ 		 		document.body.style.overflow = 'hidden';
+ 		 	}
+ 		 });
+
+ 		 popup.addEventListener('click', function(){
+ 		 		overlay.style.display = 'none';
+ 		 		document.body.style.overflow = '';
+
+ 		 });
+
+ 		 for (let i = 0; i < desc_btn.length; i++){
+ 		 	 	desc_btn[i].addEventListener('click', function(){
+ 		 		overlay.style.display = 'block';
+ 		 		document.body.style.overflow = 'hidden';
+ 		 }); 	
+ 		 }
+
+ 		 more.addEventListener('touchstart', function(e){
+ 		 		e.preventDefault();
+ 		 		overlay.classList.remove('fade');
+ 		 	 	overlay.style.display = 'block';
+ 		 		document.body.style.overflow = 'hidden';
+ 		 });
+
+function isIE() {
+	if (navigator.userAgent.indexOf("MSIE") != -1){
+		return true;
+	} else {
+		return false;
+	}
+}
+
+});
+
+
+	
